a7d6846aa6c0a6c24e540571168398a680fa1417
shanzzz321 <shanleyaninzo@gmail.com>
2025-12-03 09:14:32 -0500

    Added import in MovieTheatreApplication

--- src/main/java/com/example/oop2f25finalproject/MovieTheatreApplication.java
+++ src/main/java/com/example/oop2f25finalproject/MovieTheatreApplication.java
@@ -1,6 +1,7 @@
 package com.example.oop2f25finalproject;
 
 import com.example.oop2f25finalproject.Controllers.LoginController;
+import com.example.oop2f25finalproject.Helper.TestDataInitializer;
 import com.example.oop2f25finalproject.Model.Login;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -17,6 +18,10 @@ public class MovieTheatreApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
+        // Initialize helper
+        TestDataInitializer.initializeTestData();
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
         Parent root = fxmlLoader.load();
 
